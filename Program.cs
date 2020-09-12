b25c06cabef8ef4dcaaa6ebae1d398347da7cc0e
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-12 01:30:00 -0300

    Com todas as partes da contagem regressiva

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Contagem regressiva para largada das tartarugas");
+            Console.ResetColor();
+            Console.WriteLine(""); 
+            Console.WriteLine("Pressione uma tecla para começar...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Preparar...");
+            Console.ResetColor();
+            Console.WriteLine(""); 
+            Console.WriteLine("Pressione novamente uma tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Apontar...");
+            Console.ResetColor();
+            Console.WriteLine(""); 
+            Console.WriteLine("agora aperte com vontade qualquer tecla...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Fogo!");
+            Console.ResetColor();
+            Console.WriteLine(""); 
+            Console.WriteLine("  Quer saber qual tartaruga ganhou?");
+            Console.Write("Pressione o enter se tiver coragem!!!");
+            Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("NENHUMA, ELAS NÃO ACORDARAM NA LARGADA");
+            Console.WriteLine("    ####### HAUHAUHAUHAUHAU #######");
+            Console.ResetColor();
+
+        }
+    }
+}
